49f572cc2cccb7179dc6d796440993224d74d16e
cardosofede <federico.cardoso.e@gmail.com>
2024-04-08 18:01:47 -0300

    (feat) improve graphs

--- graphs.py
+++ graphs.py
@@ -1,6 +1,8 @@
-import streamlit as st
-import plotly.graph_objects as go
+from datetime import datetime
+
 import pandas as pd
+import plotly.graph_objects as go
+from lifelines import KaplanMeierFitter
 
 from common import schema_labels
 
@@ -10,23 +12,26 @@ WIDTH = 1500
 
 # Function to create the graph for "Patients Completing Each Treatment Line"
 def create_ultima_linea_graph(df):
+    # Counting patients in each line
     counts = df['ultima_linea'].value_counts().sort_index()
 
-    # Calculating the cumulative sum as per the specified logic
-    for i in range(1, counts.index.max() + 1):
-        counts[i] = counts.loc[i:].sum()
+    # Calculating cumulative sum in reverse order
+    cum_counts = counts[::-1].cumsum()[::-1]
 
-    # Converting counts to percentages
-    total = counts.sum()
-    counts = round((counts / total) * 100, 2)
+    # Converting to percentages
+    total_patients = df['ultima_linea'].count()
+    percentages = (cum_counts / total_patients) * 100
+
+    # Creating labels that include both percentage and count
+    labels = ["{:.1f}%\n({} patients)".format(perc, count) for perc, count in zip(percentages, cum_counts)]
 
     # Creating the plot
-    fig = go.Figure([go.Bar(x=counts.index, y=counts, text=counts, textposition='outside', marker_color='blue')])
+    fig = go.Figure([go.Bar(x=percentages.index, y=percentages, text=labels, textposition='outside', marker_color='blue')])
 
     fig.update_layout(
-        title="Patients Completing Each Treatment Line (as Percentage)",
+        title="Percentage of Patients Reaching Each Treatment Line",
         xaxis_title="Therapeutic Line",
-        yaxis_title="Percentage",
+        yaxis_title="Percentage of Patients",
         plot_bgcolor='white',
         font=dict(family="Arial, sans-serif", size=12, color="black"),
         xaxis=dict(type='category'),
@@ -37,6 +42,8 @@ def create_ultima_linea_graph(df):
     fig.update_yaxes(title_standoff=25)
 
     return fig
+
+
 def create_pie_chart_metastasic_vs_locally_advanced(df):
     counts = df['metastasico_vs_locally_advanced'].value_counts()
     fig = go.Figure([go.Pie(labels=counts.index, values=counts, textinfo='label+percent', hole=0.3)])
@@ -61,6 +68,7 @@ def create_pie_chart_ps(df):
         height=HEIGHT
     )
     return fig
+
 
 def create_pie_chart_marcador_tumoral(df):
     counts = df['marcador_tumoral'].value_counts()
@@ -100,19 +108,41 @@ def create_ps_counts_graph(df):
                       xaxis_title="Therapeutic Line", yaxis_title="Count", plot_bgcolor='white', font=dict(family="Arial, sans-serif", size=12, color="black"), width=WIDTH, height=HEIGHT)
     return fig
 
+
 # Function to create the box plot for "PLP según el Esquema de Tratamiento de 1L"
 def create_schema_plp_graph(df):
+    kmf = KaplanMeierFitter()
     fig = go.Figure()
+
     for schema in df['1L_Esquema'].unique():
         label = schema_labels.get(schema, schema)
-        fig.add_trace(go.Box(y=df[df['1L_Esquema'] == schema]['1L_PLP'], name=label))
-    fig.update_layout(title="PLP según el Esquema de Tratamiento de 1L", yaxis_title="PLP (días)",
-                      plot_bgcolor='white',
-                      font=dict(family="Arial, sans-serif", size=12, color="black"),
-                      xaxis_title="Esquema de Tratamiento 1L", width=WIDTH, height=HEIGHT)
+
+        # Subset the DataFrame for the current schema
+        subset = df[df['1L_Esquema'] == schema]
+        subset = subset.dropna(subset=['1L_fecha_inicio', '1L_fecha_progresion'])
+
+        # Event occurrence (1 if progression occurred, 0 if censored)
+        subset['event_occurred'] = ~subset['1L_fecha_progresion'].isna()
+
+        # Fit Kaplan-Meier model
+        kmf.fit(subset['duration_plp_1L'], subset['event_occurred'], label=label)
+
+        # Adding the Kaplan-Meier curve to the plot
+        fig.add_trace(go.Scatter(x=kmf.survival_function_.index, y=kmf.survival_function_[label], mode='lines', name=label))
+
+    fig.update_layout(
+        title="PLP según el Esquema de Tratamiento de 1L",
+        yaxis_title="Probabilidad de Progresión",
+        plot_bgcolor='white',
+        font=dict(family="Arial, sans-serif", size=12, color="black"),
+        xaxis_title="Tiempo (meses)",
+        width=WIDTH,
+        height=HEIGHT
+    )
+
     return fig
 
-# Function to create the box plot for "PLP para cada línea de tratamiento"
+
 def create_line_plp_graph(df):
     fig = go.Figure()
     for i in range(1, 5):
@@ -124,7 +154,7 @@ def create_line_plp_graph(df):
                       xaxis_title="Línea de Tratamiento", width=WIDTH, height=HEIGHT)
     return fig
 
-# Function to create the percentage bar chart for "Percentage of Patients Completing Each Treatment Line by First Treatment Line"
+
 def create_percentage_completion_graph(df):
     line_counts = df.groupby(['ultima_linea', '1L_Esquema']).size().unstack(fill_value=0)
     line_percentages = line_counts.divide(line_counts.sum(axis=1), axis=0) * 100
@@ -138,26 +168,38 @@ def create_percentage_completion_graph(df):
                       xaxis_title="Ultima Linea", yaxis_title="Percentage", width=WIDTH, height=HEIGHT)
     return fig
 
-def create_overall_survival_by_schema_graph(df):
-    # Filter and calculate overall survival
-    filtered_df = df[df['fecha_fallecimiento'].notna()]
-    filtered_df['overall_survival'] = (filtered_df['fecha_fallecimiento'] - filtered_df['1L_fecha_inicio']).dt.days
 
+def create_overall_survival_by_schema_graph(df):
+    kmf = KaplanMeierFitter()
     fig = go.Figure()
 
-    for schema in sorted(filtered_df['1L_Esquema'].unique()):
+    # Use current date or end of study date for censored data
+    for schema in df['1L_Esquema'].unique():
         label = schema_labels.get(schema, schema)
-        fig.add_trace(go.Box(y=filtered_df[filtered_df['1L_Esquema'] == schema]['overall_survival'], name=label))
+
+        # Subset the DataFrame for the current schema
+        subset = df[df['1L_Esquema'] == schema]
+        subset = subset.dropna(subset=['1L_fecha_inicio'])
+
+        subset["event"] = subset.apply(lambda x: x["fecha_fallecimiento"] if pd.notna(x["fecha_fallecimiento"]) else x["fecha_ultimo_contacto"], axis=1)
+        # Duration calculation
+        subset['event_occurred'] = ~subset['fecha_fallecimiento'].isna()
+        # Fit Kaplan-Meier model
+        kmf.fit(subset['duration_survival'], subset['event_occurred'], label=label)
+
+        # Adding the Kaplan-Meier curve to the plot
+        fig.add_trace(go.Scatter(x=kmf.survival_function_.index, y=kmf.survival_function_[label], mode='lines', name=label))
 
     fig.update_layout(
-        title="Overall survival (days) by 1L_Esquema. Overall Survival is calculated from 1L start date to death date.",
-        xaxis_title="1L_Esquema",
-        yaxis_title="Días",
+        title="Overall Survival by Treatment Schema",
+        yaxis_title="Probability of Survival",
         plot_bgcolor='white',
         font=dict(family="Arial, sans-serif", size=12, color="black"),
-        height=HEIGHT,
-        width=WIDTH
+        xaxis_title="Time (months)",
+        width=WIDTH,
+        height=HEIGHT
     )
+
     return fig
 
 # Function to create the box plot for "Overall Survival by PS"
