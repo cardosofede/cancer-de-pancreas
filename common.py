f5e1eb6d64408ba487039bd50476e47fb6851c11
cardosofede <federico.cardoso.e@gmail.com>
2024-04-08 14:35:38 -0300

    (feat) add schema labels in a common file

---
+++ common.py
@@ -0,0 +1,5 @@
+schema_labels = {
+            1: 'FFX', 2: 'Gem-Nab', 3: 'Gemcitabina', 4: 'Capecitabina',
+            5: 'FOLFIRI', 6: 'NALIRI', 7: 'FOLFOX', 8: 'CAPOX',
+            9: 'GEMOX', 10: 'PEMBRO', 11: 'QT_RT', 12: 'Otro', 13: 'NA'
+        }
